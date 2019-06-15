7e493a2767b216e10d639c75b26f6cb047bc9dbd
Rahul <you@example.com>
2019-06-15 18:46:54 +0530

    donate payment with readme.md

---
+++ Readme.md
@@ -0,0 +1,4 @@
+
+# donatepayment
+Create your sandbox account in paypal 
+Run this file login with your sandbox account to test this code
--- invoice.php
+++ invoice.php
@@ -14,7 +14,7 @@
   
   }
 </style>
-<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="jquery.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.2/jspdf.min.js"></script>
 
 </head>
